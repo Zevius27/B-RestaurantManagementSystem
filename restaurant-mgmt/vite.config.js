209eb054fc4a9faf68d8d14a371c5416653c21e2
Zevius27 <zeviusaplha@gmail.com>
2025-04-19 01:30:49 +0530

    rollback

--- restaurant-mgmt/vite.config.js
+++ restaurant-mgmt/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
-
+// change the port to 4000
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
